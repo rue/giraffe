b76e6815fb7cb25628e15011562919c6e715a9b0
Eero Saynatkari <projects@kittensoft.org>
2008-11-26 22:31:52 +0200

    Better default config logic.

--- environment.rb
+++ environment.rb
@@ -8,20 +8,18 @@ require 'page'
   require_gem_with_feedback gem
 end
 
-config =  begin
-            YAML.load_file ENV["GITWEB_CONFIG"]
+config = {
+          "username"    =>  nil,
+          "password"    =>  nil,
+          "repository"  => (ENV["GITWEB_REPO"] or "#{ENV["HOME"]}/wiki"),
+          "home"        => "Home",
+          # Try provide some reasonable "powered by"
+          "gitweb_home" => (`git remote -v` =~ (/origin\s+git@(.+?)\.git/) && "http://#{$1.sub ":", "/"}/" ||
+                            "http://github.com/jnewland/git-wiki/")
+         }
 
-          rescue
-            {
-              "username"    =>  nil,
-              "password"    =>  nil,
-              "repository"  => (ENV["GITWEB_REPO"] or "#{ENV["HOME"]}/wiki"),
-              "home"        => "Home",
-              # Try provide some reasonable "powered by"
-              "gitweb_home" => (`git remote -v` =~ (/origin\s+git@(.+?)\.git/) && "http://#{$1.sub ":", "/"}/" ||
-                                "http://github.com/jnewland/git-wiki/")
-            }
-          end
+config.merge! YAML.load_file(ENV["GITWEB_CONF"]) rescue nil
+
 
 # Generate some type of a link to the current git-wiki project
 GITWEB_HOME = config["gitweb_home"]
