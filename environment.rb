0c64736dc1f7328f910bef33e733739d527fa73f
Eero Saynatkari <projects@kittensoft.org>
2008-11-27 23:08:59 +0200

    Work around a ruby-git bug opening a repository.
    * ruby-git uses the working directory to compute the git directory
      as well as the index, instead of using the git directory to find
      the index.

--- environment.rb
+++ environment.rb
@@ -11,25 +11,30 @@ user_config = YAML.load_file ENV["GITWIKI_CONF"] rescue nil
 GitWiki = OpenStruct.new unless defined? GitWiki
 
 # Authentication
-GitWiki.users      = user_config["users"]
+GitWiki.users     = user_config["users"]
 
 # Repository location
-GitWiki.repo_path  = user_config["repo_path"] || ENV["GITWEB_REPO"] || "#{ENV["HOME"]}/wiki"
-GitWiki.wikiroot   = user_config["wikiroot"] || ENV["GITWEB_WIKIROOT"] || GitWiki.repository
+GitWiki.repo_path = user_config["repo_path"] || ENV["GITWEB_REPO"] || "#{ENV["HOME"]}/wiki"
+
+GitWiki.wikiroot  = user_config["wikiroot"] || ENV["GITWEB_WIKIROOT"] || GitWiki.repo_path
+GitWiki.gitdir    = user_config["git_dir"] || "#{GitWiki.repo_path}/.git"
+GitWiki.index     = user_config["git_index"] || "#{GitWiki.gitdir}/index"
 
 # Wiki setup
-GitWiki.home       = user_config["home"] || "/Home"
+GitWiki.home      = user_config["home"] || "/Home"
 
 # Some type of a link to git-wiki version used
-GitWiki.itself     = user_config["gitwiki_page"] ||
-                    `git remote -v` =~ (/origin\s+git@(.+?)\.git/) && "http://#{$1.sub ":", "/"}/" ||
-                    "http://github.com/rue/git-wiki/"
+GitWiki.itself    = user_config["gitwiki_page"] ||
+                   `git remote -v` =~ (/origin\s+git@(.+?)\.git/) && "http://#{$1.sub ":", "/"}/" ||
+                   "http://github.com/rue/git-wiki/"
 
 # Git needs this.
 require "fileutils"
 
 begin
-  GitWiki.repo = Git.open GitWiki.wikiroot, :repository => GitWiki.repo_path
+  GitWiki.repo = Git.open GitWiki.wikiroot,
+                          :repository => GitWiki.gitdir,
+                          :index => GitWiki.index
 rescue
   puts "Initializing repository in #{GitWiki.repo_path}."
   GitWiki.repo = Git.init GitWiki.repo_path
