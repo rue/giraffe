54b0771c5c1bd4c14feea653b4c91e5e1ba083db
Eero Saynatkari <projects@kittensoft.org>
2008-11-28 08:54:12 +0200

    Doc resource maps for the look of the thing.

--- bin/git-wiki.rb
+++ bin/git-wiki.rb
@@ -16,60 +16,75 @@ require "rubygems"
   require "rubypants"
 
 
+# Authentication.
+#
 before do
   authenticate {|user, pass| GitWiki.users[user] == pass } if GitWiki.users
 end
 
-# Resource mapping
+
+  # Resource mapping
 
 
-# Default
+# Default page.
+#
 get('/') { redirect GitWiki.home }
 
 
-# Pages
+# Show page or send to edit if it does not exist.
+#
 get '/:page' do
-  @page = Page.new(params[:page])
-  @page.tracked? ? show(:show, @page.name) : redirect('/e/' + @page.name)
+  @page = Page.new params[:page]
+  if @page.tracked? then show(:show, @page.name) else redirect('/e/' + @page.name) end
 end
 
-# Raw page text
-get('/:page/raw') { redirect "/#{params[:page]}.txt" }
-
-get "/:page.txt" do
+# Give user the raw source text of the page.
+#
+get "/:page/raw" do
   headers 'Content-Type' => 'text/plain;charset=utf-8'
   @page = Page.new(params[:page])
   send_data @page.raw_body, :type => 'text/plain', :disposition => 'inline'
 end
 
-# Page editing
+# Page editor.
+#
 get '/e/:page' do
   @page = Page.new(params[:page])
   show :edit, "Editing #{@page.name}"
 end
 
+# Process edit.
+#
 post '/e/:page' do
   @page = Page.new(params[:page])
   @page.update(params[:body], params[:message])
   redirect '/' + @page.name
 end
 
+# Process in-place edit.
+#
 post '/eip/:page' do
   @page = Page.new(params[:page])
   @page.update(params[:body])
   @page.body
 end
 
+# Show page history.
+#
 get '/h/:page' do
   @page = Page.new(params[:page])
   show :page_history, "History of #{@page.name}"
 end
 
+# Show page history for given revision.
+#
 get '/h/:page/:rev' do
   @page = Page.new(params[:page], params[:rev])
   show :show, "#{@page.name} (version #{params[:rev]})"
 end
 
+# Raw history for given revision.
+#
 get '/h/:page/:rev.txt' do
   @page = Page.new(params[:page], params[:rev])
   send_data @page.raw_body, :type => 'text/plain', :disposition => 'inline'
