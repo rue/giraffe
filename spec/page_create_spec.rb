1cad05d06ae3e459978a702760035d03f980cad7
Eero Saynatkari <projects@kittensoft.org>
2008-12-13 07:00:39 +0200

    Spec new page creation using PUT on page resources.

---
+++ spec/page_create_spec.rb
@@ -0,0 +1,46 @@
+require "spec/spec_helper"
+
+
+describe "Creating a new page resource with PUT" do
+
+  before :each do
+    create_good_repo
+
+    Waves << Giraffe
+  end
+
+  after :each do
+    delete_good_repo
+
+    Waves.applications.clear
+  end
+
+  it "redirects to the page resource" do
+    response = put("/file0", :input => "contents=Bleh%20this%20sucks&message=whatever")
+
+    response.status.should == 302
+    response.location.should == "/file0"
+  end
+
+  it "commits the update into the repository indicating new file" do
+    old = Dir.chdir(@repo) { `git show HEAD 2>&1` }
+
+    response = put("/file0", :input => "contents=w00t&message=Cool%20#{@proc}%20message")
+
+    noob = Dir.chdir(@repo) { `git show HEAD 2>&1` }
+
+    noob.should_not == old
+    noob.should =~ /Cool #{@proc} message\s*$\s*Giraffe created file0.txt/im
+  end
+
+  it "shows the updated resource when having redirected" do
+    put "/file0", :input => "contents=w00t&message=Cool%20#{@proc}%20message"
+
+    noob = Nokogiri::HTML.parse(get("/file0").body).css("#body").first.content
+
+    noob.should =~ /w00t/
+    noob.should_not =~ /cool\s+#{@proc}\s+message/i
+  end
+
+end
+
